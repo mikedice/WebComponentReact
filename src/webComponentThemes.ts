5e352076cfe3adbac03f857748b7bda1d786de24
Mike Dice <mikedice417@hotmail.com>
2023-07-02 07:48:08 -0700

    updates

--- src/webComponentThemes.ts
+++ src/webComponentThemes.ts
@@ -1,14 +1,26 @@
-import { StandardLuminance, baseLayerLuminance} from '@fluentui/web-components';
+import { StandardLuminance, baseLayerLuminance, bodyFont , typeRampPlus4FontSize} from '@fluentui/web-components';
 
 
 export function ThemeComponents(){
 
     // Set theme and configure the button
     const button = document.getElementById("clickMe");
+    let lumLight = true;
     baseLayerLuminance.setValueFor(button!, StandardLuminance.LightMode);
+    var bodies = document.getElementsByName("body");
+    var font = bodyFont.getValueFor(bodies[0]);
+
+  // bodyFont.setValueFor(button! , typeRampPlus4FontSize);
+
     button?.addEventListener("click", () => { 
         console.log("Hello World!");
+        baseLayerLuminance.setValueFor(button!, lumLight === true ? StandardLuminance.DarkMode : StandardLuminance.LightMode);
+        lumLight = !lumLight;
     });
+
+    const card = document.getElementById("myCard");
+    bodyFont.setValueFor(card!, "Segoe UI");
+
 }
 
 ThemeComponents();
